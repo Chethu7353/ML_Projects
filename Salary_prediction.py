3138342131dbede8380f3b6cd57c59b87edccb96
A Chetan Kumar <vichetan79@gmail.com>
2026-03-11 18:11:02 +0530

    Update Salary_prediction.py

--- Salary_prediction.py
+++ Salary_prediction.py
@@ -3,19 +3,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-df=pd.read_csv("Salary_dataset.csv")
-x=df.iloc[:,0:1]
-y=df.iloc[:,-1]
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=2)
-plt.scatter(df["YearsExperience"],df["Salary"])
+
+df = pd.read_csv("Salary_dataset.csv")
+
+x = df[['YearsExperience']]
+y = df['Salary']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+
+plt.scatter(df["YearsExperience"], df["Salary"])
 plt.xlabel("Years")
 plt.ylabel("Salary")
-lr=LinearRegression()
-lr.fit(x_train,y_train)
-year=float(input("Enter the No year Expereince : "))
+
+lr = LinearRegression()
+lr.fit(x_train, y_train)
+
+year = float(input("Enter the No year Experience : "))
+
 p = lr.predict(pd.DataFrame([[year]], columns=['YearsExperience']))
-m=lr.coef_
-c=lr.intercept_
-print("The slop of the regression line: ",m)
-print("The intercept of the graph :",c)
-print("The expecting Salary is :",p)
+
+m = lr.coef_
+c = lr.intercept_
+
+print("The slope of the regression line:", m)
+print("The intercept of the graph:", c)
+print("The expected Salary is:", p[0])
